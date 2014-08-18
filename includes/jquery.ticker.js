a3c595bb5662135070f0203ac3e52a7d7f12d729
laflamablanca <BB@Brandons-MacBook-Air.local>
2014-08-18 12:04:36 -0400

    change feed

--- includes/jquery.ticker.js
+++ includes/jquery.ticker.js
@@ -412,7 +412,7 @@
 	$.fn.ticker.defaults = {
 		speed: 0.10,			
 		ajaxFeed: true,
-		feedUrl: 'includes/nprfeed.rss',
+		feedUrl: 'includes/testfeed.rss',
 		feedType: 'xml',
 		displayType: 'reveal',
 		htmlFeed: false,
